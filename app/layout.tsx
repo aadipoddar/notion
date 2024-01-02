4fb7b840777cf60c5c0debea166452f484c27e41
Aadi Poddar <aadipoddarmail@gmail.com>
2024-01-03 01:23:38 +0530

    Search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
+import { Toaster } from "sonner";
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
-import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Jotion',
-  description: 'The connected workspace where better faster work happens',
+  description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
       {
         media: "(prefers-color-scheme: light)",
         url: "/logo.svg",
-        href: "/logo.svg"
+        href: "/logo.svg",
       },
       {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
-        href: "/logo-dark.svg"
+        href: "/logo-dark.svg",
       }
     ]
   }
@@ -36,8 +36,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <ConvexClientProvider>
           <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
+            attribute="class"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
             storageKey="jotion-theme-2"
--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,24 +1,27 @@
-"use client"
+"use client";
 
-import { ClerkProvider, useAuth } from "@clerk/clerk-react"
-import { ConvexReactClient } from "convex/react"
-import { ConvexProviderWithClerk } from "convex/react-clerk"
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const ConvexClientProvider = ({
-    children
+  children
 }: {
-    children: ReactNode
+  children: ReactNode;
 }) => {
-    return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-            <ConvexProviderWithClerk
-                useAuth={useAuth}
-                client={convex}>
-                {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    )
-}
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk
+        useAuth={useAuth}
+        client={convex}
+      >
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
