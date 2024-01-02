374292d9d970995fdf611312b58c977d7e9abdf1
Aadi Poddar <aadipoddarmail@gmail.com>
2024-01-03 00:08:10 +0530

    Trash Box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -20,6 +20,8 @@ import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
     const router = useRouter();
@@ -163,6 +165,23 @@ export const Navigation = () => {
 
                 <div className="mt-4">
                     <DocumentList />
+                    <Item
+                        onClick={handleCreate}
+                        icon={Plus}
+                        label="Add a page"
+                    />
+                    <Popover>
+                        
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                            className="p-0 w-72"
+                            side={isMobile ? "bottom" : "right"}
+                        >
+                            <TrashBox />
+                        </PopoverContent>
+                    </Popover>
                 </div>
 
                 <div
